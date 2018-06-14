e3607375b7019521275375817408221ef635f183
KOSTA <KOSTA@192.168.0.135>
2018-06-14 16:48:30 +0900

    travelController

--- ex21_buyTrip_bootstrap/src/main/java/buytrip/mvc/controller/TravelController.java
+++ ex21_buyTrip_bootstrap/src/main/java/buytrip/mvc/controller/TravelController.java
@@ -15,51 +15,51 @@ import buytrip.mvc.model.travel.service.TravelService;
 @RequestMapping("travel")
 public class TravelController {
 
-	@Autowired
-	private TravelService travelService;
-	
-	
-	/**
-	 * addTrip 폼에 정보 기입하여 여행일정 즐겨찾기 추가하기 (알림기능:메세지,여행지의새상품)
-	 */
-	@RequestMapping(value="/insert",produces="application/json;charset=UTF-8")
-	@ResponseBody
-	public int insertTravel(TravelDTO travelDTO) {
-		
-		return travelService.insert(travelDTO);
-	}
-	
-	/**
-	 * [mypage] 등록한 여행일정 삭제하기
-	 */
-	@RequestMapping(value="delete",produces="application/json;charset=UTF-8")
-	@ResponseBody
-	public int deleteTravel(String travelNo){
-		
-		System.out.println("controller = "+travelNo);
-		return travelService.delete(travelNo);
-		
-	}
-	
-	
-	/**
-	 * [mypage] 등록한 여행일정 중 기한만료된 list 보기
-	 */
-	@RequestMapping("selectPast")
-	@ResponseBody
-	public List<TravelDTO> selectPast(){
-		return travelService.selectPast();
-	}
-	
-	@RequestMapping("suggest")
-	@ResponseBody
-	public List<String> suggest(String keyWord) {
-		
-		List<String> list = travelService.suggest(keyWord);
-		
-		return list;
-		
-	}
+   @Autowired
+   private TravelService travelService;
+   
+   
+   /**
+    * addTrip 폼에 정보 기입하여 여행일정 즐겨찾기 추가하기 (알림기능:메세지,여행지의새상품)
+    */
+   @RequestMapping(value="/insert",produces="application/json;charset=UTF-8")
+   @ResponseBody
+   public int insertTravel(TravelDTO travelDTO) {
+      
+      return travelService.insert(travelDTO);
+   }
+   
+   /**
+    * [mypage] 등록한 여행일정 삭제하기
+    */
+   @RequestMapping(value="delete",produces="application/json;charset=UTF-8")
+   @ResponseBody
+   public int deleteTravel(String travelNo){
+      
+      System.out.println("controller = "+travelNo);
+      return travelService.delete(travelNo);
+      
+   }
+   
+   
+   /**
+    * [mypage] 등록한 여행일정 중 기한만료된 list 보기
+    */
+   @RequestMapping("selectPast")
+   @ResponseBody
+   public List<TravelDTO> selectPast(){
+      return travelService.selectPast();
+   }
+   
+   @RequestMapping("suggest")
+   @ResponseBody
+   public List<String> suggest(String keyWord) {
+      
+      List<String> list = travelService.suggest(keyWord);
+      
+      return list;
+      
+   }
 
-	
+   
 }
